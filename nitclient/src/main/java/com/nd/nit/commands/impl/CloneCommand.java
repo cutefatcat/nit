b1dabb41834e35d764761132e937bf8f146d1be1
Natalia <natalia.voblikova@gmail.com>
2018-06-28 00:36:45 +0300

    Update CloneCommand.
    Common library extracted.

--- nitclient/src/main/java/com/nd/nit/commands/impl/CloneCommand.java
+++ nitclient/src/main/java/com/nd/nit/commands/impl/CloneCommand.java
@@ -1,11 +1,22 @@
 package com.nd.nit.commands.impl;
 
 import com.nd.nit.commands.Command;
+import com.nd.nit.models.CreateVersionModel;
+import com.nd.nit.models.FileBinaryModel;
+import com.nd.nit.models.FileInfoModel;
+import com.nd.nit.models.VersionModel;
+import org.apache.commons.io.IOUtils;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
+import sun.nio.ch.IOUtil;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class CloneCommand implements Command{
     private String url;
@@ -42,9 +53,43 @@ public class CloneCommand implements Command{
         TODO get file one by one
          */
 
+        List<FileInfoModel> fileInfoList = new ArrayList<>();
+        fileInfoList = getFileInfoList();
+        int fileBinaryId;
+
+        for (int i = 0; i < fileInfoList.size(); i++){
+            fileBinaryId = fileInfoList.get(i).getBinaryId();
+            getFile(fileBinaryId);
+        }
+    }
+
+    public List<FileInfoModel> getFileInfoList(){
         RestTemplate restTemplate = new RestTemplate();
-       // VersionModel = restTemplate.getForObject("http://localhost:8080/file", Quote.class);
+        CreateVersionModel createVersionModel  = restTemplate.getForObject("http://localhost:8080/version/16",CreateVersionModel.class);
 
+        FileInfoModel fileInfoModel = new FileInfoModel();
+        List<FileInfoModel> fileInfoModelList = new ArrayList<>();
+        fileInfoModelList = createVersionModel.getInfoModelList();
 
+        return fileInfoModelList;
+    }
+
+    public ResponseEntity<Resource> getFile(int fileId){
+        RestTemplate restTemplate = new RestTemplate();
+        String url = String.format("http://localhost:8080/file/%s", fileId);
+
+        HttpHeaders headers = new HttpHeaders();
+        //headers.setAccept(Arrays.asList(MediaType.APPLICATION_OCTET_STREAM));
+        HttpEntity<String> entity = new HttpEntity<String>(headers);
+        ResponseEntity<Resource> responseEntity = restTemplate.exchange(url, HttpMethod.GET, entity, Resource.class);
+        try (OutputStream outputStream = new FileOutputStream(new File("file.txt"))) {
+            IOUtils.copy(responseEntity.getBody().getInputStream(), outputStream);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return responseEntity;
     }
 }
