3570c38217e1282523f3e010a2c35f7a75b382c9
Natalia <natalia.voblikova@gmail.com>
2018-07-11 21:21:10 +0300

    VersionsCommand fixed.

--- nitclient/src/main/java/com/nd/nit/commands/impl/VersionsCommand.java
+++ nitclient/src/main/java/com/nd/nit/commands/impl/VersionsCommand.java
@@ -5,22 +5,12 @@ import com.nd.nit.models.ListVersionModel;
 import com.nd.nit.models.VersionModel;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Collections;
 import java.util.List;
 
 public class VersionsCommand extends BaseCommand implements Command{
-
     @Override
     public void execute() {
-        //VersionModel version = getVersionsList();
         List<VersionModel> versionsList = getVersionsList();
-//        for (VersionModel version : versionsList){
-//            try {
-//                downloadFile(fileInfo);
-//            }// catch (IOException e) {
-//               // System.out.println("Error during " + version.getName() + " file downloading. " + e.getMessage());
-//            //}
-//        }
         printVersions(versionsList);
     }
 
@@ -35,12 +25,9 @@ public class VersionsCommand extends BaseCommand implements Command{
 
         RestTemplate restTemplate = new RestTemplate();
         String url = baseUrl + "/version";
-       // List<VersionModel> versionsList = restTemplate.getForObject(url, List.class);
 
         ListVersionModel model  = restTemplate.getForObject(url, ListVersionModel.class);
         List<VersionModel> versionsList = model.getListVersions();
-
-
 
         return versionsList;
     }
@@ -51,19 +38,16 @@ public class VersionsCommand extends BaseCommand implements Command{
         System.out.println("├──────┼─────────────────────────┼─────────────────────────────────────────────────────────┤");
 
         for (VersionModel version : versionsList) {
-            //System.out.printf("|%d  |%t  |%s  |", version.getId(), version.getCreateDate(), version.getDescription());
             if(version.getId() < 10){
                 System.out.printf("  %d   |", version.getId());
             } else if (version.getId() < 100){
                 System.out.printf("  %d  |", version.getId());
-            } else if (version.getId() < 1000){
+            } else {
                 System.out.printf("  %d |", version.getId());
             }
 
             System.out.print(" " + version.getCreateDate() + "  ");
-            //System.out.printf("|%t  |", version.getCreateDate());
             System.out.printf("| %s %n", version.getDescription());
-
 
             if (versionsList.lastIndexOf(version) == versionsList.size()-1) {
                 System.out.println("└──────┴─────────────────────────┴─────────────────────────────────────────────────────────┘");
